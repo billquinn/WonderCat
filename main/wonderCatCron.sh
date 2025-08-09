bb27ebcce1875c0f6c04935953ebf684850dced3
Bill Quinn <wreedquinn@gmail.com>
2025-08-09 10:14:57 -0400

    Updating cron jobs to continue indefinitely.

--- main/wonderCatCron.sh
+++ main/wonderCatCron.sh
@@ -5,6 +5,6 @@ wonderCRON_JOB="*/10 * * * * /usr/bin/python3 /root/shiny-server/apps/sample-app
 # Check if the cron job already exists to avoid duplication
 (crontab -l | grep -v -F "$wonderCRON_JOB"; echo "$wonderCRON_JOB") | crontab -
 echo "✅ Scheduled cron job to run every 10 minutes."
-# Schedule a one-time cron job to remove it at midnight
-(crontab -l; echo "59 23 * * * crontab -r") | crontab -
-echo "✅ Scheduled removal of cron job at midnight."
+# # Schedule a one-time cron job to remove it at midnight
+# (crontab -l; echo "59 23 * * * crontab -r") | crontab -
+# echo "✅ Scheduled removal of cron job at midnight."
